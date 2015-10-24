7d166853d0b29738b85419043ff66c919ffe1e6a
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 11:19:46 -0600

    Attribute Overrides
    Add more overrides for krb5 attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
-override['krb5']['krb5_conf']['realms']['default_realm_kdcs'] =
-  %w(orion.jmorgan.org)
+override['krb5']['krb5_conf']['realms'] = {
+  'default_realm' => default['krb5']['krb5_conf']['realms']['default_realm'],
+  'default_realm_kdcs' => %w(orion.jmorgan.org),
+  'default_realm_admin_server' => 'orion.jmorgan.org',
+  'realms' => [default['krb5']['krb5_conf']['realms']['default_realm']]
+}
