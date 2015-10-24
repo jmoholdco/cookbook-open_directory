ce733e100b5a6cea1543e8a3b8e37731668b0cd6
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 11:40:15 -0600

    Passwords Recipe Specs
    Added specs for a to-be-written 'passwords' recipe that will load the
    passwords for krb5 master_password and admin_password from a vault,
    then set the node's attributes.

---
+++ spec/unit/recipes/passwords_spec.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: open_directory
+# Spec:: default
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 J. Morgan Lieberthal
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'spec_helper'
+
+RSpec.describe 'open_directory::passwords' do
+  let(:chef_run) { ChefSpec::SoloRunner.new(opts).converge(described_recipe) }
+  {
+    'centos' => %w(7.0 7.1.1503),
+    'debian' => %w(7.8 8.0 8.1)
+  }.each do |platform, versions|
+    versions.each do |version|
+      context "on #{platform} v#{version}" do
+        let(:opts) { { platform: platform, version: version } }
+        include_examples 'converges successfully'
+        before :each do
+          expect(chef_run.node['krb5']['master_password']).to eq 'password'
+          expect(chef_run.node['krb5']['admin_password']).to eq 'password'
+        end
+
+        it 'sets the master password from the vault' do
+          expect(chef_run.node['krb5']['master_password']).to eq 'masterpass'
+        end
+
+        it 'sets the admin password from the vault' do
+          expect(chef_run.node['krb5']['admin_password']).to eq 'adminpass'
+        end
+      end
+    end
+  end
+end
