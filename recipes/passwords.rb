42157b07b53090dc6ca91a6efff6765792efa1e0
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 11:49:06 -0600

    Implement the passwords recipe

--- recipes/passwords.rb
+++ recipes/passwords.rb
@@ -23,3 +23,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
+
+include_recipe 'chef-vault'
+
+password_vault = chef_vault_item('passwords', 'krb5')
+
+node.override['krb5']['master_password'] = password_vault['master']
+node.override['krb5']['admin_password'] = password_vault['admin']
