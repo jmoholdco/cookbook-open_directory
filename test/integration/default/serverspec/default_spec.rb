f8aa4d610e336aa76e9071881c559e4e4516d184
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 11:31:20 -0600

    Add first round of integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe 'open_directory::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe 'krb5 installation and setup' do
+    describe file('/etc/krb5.conf') do
+      it { is_expected.to be_file }
+      it { is_expected.to exist }
+      it 'has the right configuration' do
+        expect(subject.content).to match(
+          /\[libdefaults\]\s+^\s+default_realm = JMORGAN\.ORG/
+        )
+        expect(subject.content).to match(
+          /\[realms\]\s+JMORGAN\.ORG = \{\n\s+kdc = orion\.jmorgan\.org\n\s\}/
+        )
+      end
+    end
   end
 end
