2e2b7f3e6b0d3a1b85e7a921e1ed34497dc9b7e2
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-24 14:29:31 -0600

    Additional Integration Specs
    Add specs to make sure all principals are properly created.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -12,5 +12,28 @@ RSpec.describe 'open_directory::default' do
         expect(subject.content).to match(/\[realms\]\s+JMORGAN\.ORG = /)
       end
     end
+
+    kdc_service = os[:family] == 'redhat' ? 'krb5kdc' : 'krb5-kdc'
+    kadmin_service = os[:family] == 'redhat' ? 'kadmin' : 'krb5-admin-server'
+
+    describe service(kdc_service) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe service(kadmin_service) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe command('kadmin.local -p admin/admin -q "listprincs"') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) { is_expected.to match %r{host/orion\.jmorgan\.org} }
+      its(:stdout) { is_expected.to match %r{admin/admin@JMORGAN\.ORG} }
+      its(:stdout) { is_expected.to match %r{kadmin/admin@JMORGAN\.ORG} }
+      its(:stdout) { is_expected.to match %r{kadmin/changepw@JMORGAN\.ORG} }
+      its(:stdout) { is_expected.to match %r{kadmin/orion\.jmorgan\.org} }
+      its(:stdout) { is_expected.to match %r{krbtgt/JMORGAN\.ORG@JMORGAN\.ORG} }
+    end
   end
 end
